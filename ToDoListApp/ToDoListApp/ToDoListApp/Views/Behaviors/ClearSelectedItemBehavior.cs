4d61046aeb2c7aaac42e7d2a5c8373e9282a4a38
JoaoCosta37 <joao.costa37@hotmail.com>
2022-10-29 00:09:15 -0300

    Resolução de Exercício

---
+++ ToDoListApp/ToDoListApp/ToDoListApp/Views/Behaviors/ClearSelectedItemBehavior.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace ToDoListApp.Views.Behaviors
+{
+    public class ClearSelectedItemBehavior : Behavior<ListView>
+    {
+        protected override void OnAttachedTo(ListView bindable)
+        {
+            base.OnAttachedTo(bindable);
+
+            bindable.ItemSelected += Bindable_ItemSelected;
+        }
+
+        private void Bindable_ItemSelected(object sender, SelectedItemChangedEventArgs e)
+        {
+            (sender as ListView).SelectedItem = null;
+
+        }
+    }
+}
--- ToDoListApp/ToDoListApp/ToDoListApp/Views/ToDoListPage.xaml.cs
+++ ToDoListApp/ToDoListApp/ToDoListApp/Views/ToDoListPage.xaml.cs
@@ -17,9 +17,27 @@ namespace ToDoListApp.Views
             InitializeComponent();
         }
 
+    }
+
+    public class DeselectItemBehavior : Behavior<ListView>
+    {
+        protected override void OnAttachedTo(ListView bindable)
+        {
+            base.OnAttachedTo(bindable);
+
+            bindable.ItemSelected += ListView_ItemSelected;
+        }
+
+        protected override void OnDetachingFrom(ListView bindable)
+        {
+            base.OnDetachingFrom(bindable);
+
+            bindable.ItemSelected -= ListView_ItemSelected;
+        }
+
         private void ListView_ItemSelected(object sender, SelectedItemChangedEventArgs e)
         {
-
+            ((ListView)sender).SelectedItem = null;
         }
     }
 }
