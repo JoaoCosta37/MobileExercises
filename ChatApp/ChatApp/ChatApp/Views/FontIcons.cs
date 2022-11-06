6443fcb8be2dc7d4a77526661db95a45fa190c05
JoaoCosta37 <109122261+JoaoCosta37@users.noreply.github.com>
2022-11-06 20:55:27 -0300

    Bug
    Não aparece o arquivo na Soluction para poder incluir no projeto (Pasta Assets
    )

---
+++ ChatApp/ChatApp/ChatApp/Views/FontIcons.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ChatApp.Views
+{
+    public class FontIcons
+    {
+
+    }
+        public static class ChatIcons
+        {
+            public static string PlusIcon = "\uE800";
+            public static string XIcon = "\uE801";
+            public static string ChatIcon = "\uF0E6";
+            public static string SendIcon = "\uE802";
+        }
+}
